56d1bb388b309143d724d167d2c69cd1d665a5c6
Jajo2003 <116101888+Jajo2003@users.noreply.github.com>
2023-06-07 16:24:28 +0400

    Add files via upload

---
+++ memory game/app.js
@@ -0,0 +1,110 @@
+const cards = [
+  {
+    name:'fries',
+    img:'./images/fries.png',
+  },
+  
+  {
+    name:'cheeseburger',
+    img:'./images/cheeseburger.png'
+  },
+  {
+    name:'hotdog',
+    img:'./images/hotdog.png'
+  },
+  {
+    name:'ice-cream',
+    img:'./images/ice-cream.png'
+  },
+  {
+    name:'milkshake',
+    img:'./images/milkshake.png'
+  },
+  {
+    name:'pizza',
+    img:'./images/pizza.png'
+  },
+
+  {
+    name:'fries',
+    img:'./images/fries.png',
+  },
+  
+  {
+    name:'cheeseburger',
+    img:'./images/cheeseburger.png'
+  },
+  {
+    name:'hotdog',
+    img:'./images/hotdog.png'
+  },
+  {
+    name:'ice-cream',
+    img:'./images/ice-cream.png'
+  },
+  {
+    name:'milkshake',
+    img:'./images/milkshake.png'
+  },
+  {
+    name:'pizza',
+    img:'./images/pizza.png'
+  },
+
+]
+cards.sort(()=> 0.5 - Math.random());
+const grid =document.getElementById('grid');
+
+function board(){
+  for(let i=0;i<cards.length;i++){
+    const card = document.createElement('img');
+    card.setAttribute('src','images/blank.png');
+    card.setAttribute('data-id',i);
+    card.addEventListener('click',Flip)
+    grid.append(card);
+  }
+}
+let cardswon = [];
+let chosencards = [];
+let chosecardsid = [];
+function checkmatch(){
+  const kartebi=document.querySelectorAll('img');
+  if(chosencards[0] === chosencards[1]){
+    kartebi[chosecardsid[0]].setAttribute('src','images/white.png');
+    kartebi[chosecardsid[1]].setAttribute('src','images/white.png');
+    kartebi[chosecardsid[0]].removeEventListener('click',Flip);
+    kartebi[chosecardsid[1]].removeEventListener('click',Flip);
+    cardswon.push(chosencards);
+  }
+  else{
+    kartebi[chosecardsid[0]].setAttribute('src','images/blank.png');
+    kartebi[chosecardsid[1]].setAttribute('src','images/blank.png');
+    
+  }
+  checkwin();
+  chosencards = [];
+    chosecardsid = [];
+}
+board();
+function Flip(){
+  const cardid=this.getAttribute('data-id');
+  this.setAttribute('src',cards[cardid].img);
+  chosencards.push(cards[cardid].name);
+  chosecardsid.push(cardid)
+  console.log('Clickeed',cardid);
+  if(chosencards.length === 2){
+    setTimeout(checkmatch,400);
+  }
+}
+function checkwin(){
+  if(cardswon.length === cards.length/2){
+    setTimeout(winner,2000);
+    setTimeout(rickroll,2000);
+  }
+}
+function winner(){
+  document.body.innerHTML='Moigeee';
+}
+function rickroll(){
+location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
+}
