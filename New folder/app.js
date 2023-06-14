e65fd648636ba27c92b8d21684a79b18e7d40570
jeiji <jeiji@gmail.com>
2023-06-14 21:19:47 +0400

    commit

--- New folder/app.js
+++ New folder/app.js
@@ -2,17 +2,33 @@ const array = [
         
         'picsandmp3/2pac.jpg',
         'picsandmp3/asap-rocky.jpg',
-        'picsandmp3/ice-cube.jpg',
         'picsandmp3/ricardo-milos.jpg',
-    
+        'picsandmp3/ice-cube.jpg',
+        'picsandmp3/pucci.jpg',
 
 ]
 const pics = [];
-const id = [];
 for(let i=0;i<array.length;i++){
     let pic= `<img src = "${array[i]}" class="pic">`;
     pics[i]=pic;
-    id[i]=i;
+}
+function pause_play(){
+    const pauseOrPlay=document.getElementById('switch');
+    if(pauseOrPlay.classList.contains('fa-play')){
+        pauseOrPlay.classList.remove('fa-play');
+        pauseOrPlay.classList.add('fa-pause');
+       
+    }
+    else if(pauseOrPlay.classList.contains('fa-pause')){
+        pauseOrPlay.classList.remove('fa-pause');
+        pauseOrPlay.classList.add('fa-play');
+    }
+    
+}
+function aaa(value){
+    let a=document.getElementById('pic-cont');
+    a.innerHTML=pics[value];
+    
 }
 
 
